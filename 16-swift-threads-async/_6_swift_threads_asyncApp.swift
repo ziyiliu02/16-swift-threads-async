b804f0c1e45d1b29b4b7b33f71a3808d35fd1788
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-03 00:16:41 +0800

    - Firebase Setup

--- 16-swift-threads-async/_6_swift_threads_asyncApp.swift
+++ 16-swift-threads-async/_6_swift_threads_asyncApp.swift
@@ -5,10 +5,16 @@
 //  Created by Liu Ziyi on 2/9/23.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct _6_swift_threads_asyncApp: App {
+    
+    init() {
+        FirebaseApp.configure() 
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
