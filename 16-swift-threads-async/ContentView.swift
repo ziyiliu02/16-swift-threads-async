35beb71d27cb79c4aebc1fbf7cbc5943b04d1fd9
Liu Ziyi <ziyi.liu02@gmail.com>
2023-09-02 23:46:03 +0800

    Initial Commit

---
+++ 16-swift-threads-async/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  16-swift-threads-async
+//
+//  Created by Liu Ziyi on 2/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 16-swift-threads-async/_6_swift_threads_asyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_swift_threads_asyncApp.swift
+//  16-swift-threads-async
+//
+//  Created by Liu Ziyi on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct _6_swift_threads_asyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
